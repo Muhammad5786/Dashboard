3a5ab29a81b6b5b0e8e37041b0645241d6a8c30c
Frantically <2060apollo@gmail.com>
2025-06-19 12:07:11 +0700

    fix aksi_simpan_register

--- php/aksi_simpan_register.php
+++ php/aksi_simpan_register.php
@@ -7,7 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $confirm  = $_POST['confirm_password'];
 
     // Cek apakah username sudah digunakan
-    $cek = mysqli_query($conn, "SELECT * FROM users WHERE username = '$username'");
+    $cek = mysqli_query($conn, "SELECT * FROM user WHERE nama = '$username'");
     if (mysqli_num_rows($cek) > 0) {
         echo "<script>alert('Username sudah digunakan!'); window.location.href = '../pages/register.php';</script>";
         exit;
@@ -21,9 +21,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Enkripsi password sebelum disimpan
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-    $query = "INSERT INTO users (username, password) VALUES ('$username', '$hashed_password')";
+    $query = "INSERT INTO user (nama, password) VALUES ('$username', '$hashed_password')";
     if (mysqli_query($conn, $query)) {
-        echo "<script>alert('Registrasi berhasil! Silakan login.'); window.location.href = '../login.php';</script>";
+        echo "<script>alert('Registrasi berhasil! Silakan login.'); window.location.href = '../pages/login.php';</script>";
         exit;
     } else {
         echo "Error: " . mysqli_error($conn);
